f4e616de0bdd451257410c5f0e77c14036abc0c7
chung <chung305>
2019-12-31 17:52:28 -0500

    login design done

--- FlipCoin/src/app/app-routing.module.ts
+++ FlipCoin/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountListComponent } from './account-list/account-list.component'
 import { AccountFormComponent } from './account-form/account-form.component';
 import { AccountOverviewComponent } from './account-overview/account-overview.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'accounts', component: AccountListComponent },
   { path: 'addaccounts', component: AccountFormComponent },
-  { path: 'accountsOverview/:accountId', component: AccountOverviewComponent},
+  { path: 'accountsOverview/:accountId', component: AccountOverviewComponent}
+  
+  
  
   
 ];
