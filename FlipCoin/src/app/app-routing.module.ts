48ee3fee54d502932b63814a4f97cd628fed80b2
chung <chung305>
2020-01-01 12:43:27 -0500

    added paths to about/help/contact

--- FlipCoin/src/app/app-routing.module.ts
+++ FlipCoin/src/app/app-routing.module.ts
@@ -4,9 +4,15 @@ import { AccountListComponent } from './account-list/account-list.component'
 import { AccountFormComponent } from './account-form/account-form.component';
 import { AccountOverviewComponent } from './account-overview/account-overview.component';
 import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './info/contact/contact.component';
+import { AboutUsComponent } from './info/about-us/about-us.component';
+import { HelpComponent } from './info/help/help.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent},
+  { path: 'help', component: HelpComponent },
   { path: 'accounts', component: AccountListComponent },
   { path: 'addaccounts', component: AccountFormComponent },
   { path: 'accountsOverview/:accountId', component: AccountOverviewComponent}
--- FlipCoin/src/app/app.module.ts
+++ FlipCoin/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { FooterComponent } from './layoutComponents/footer/footer.component';
 import { HeaderComponent } from './layoutComponents/header/header.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { AboutUsComponent } from './info/about-us/about-us.component';
+import { ContactComponent } from './info/contact/contact.component';
+import { HelpComponent } from './info/help/help.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,10 @@ import { RouterModule } from '@angular/router';
     AccountOverviewComponent,
     FooterComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    AboutUsComponent,
+    ContactComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
