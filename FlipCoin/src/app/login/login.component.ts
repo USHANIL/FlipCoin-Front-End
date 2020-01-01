a4295438e086348b56c87d7177716275daa60fa9
chung <chung305>
2020-01-01 15:56:43 -0500

    implemented log in,log out, and fixed the logged in dashboard

--- FlipCoin/src/app/login/login.component.ts
+++ FlipCoin/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'app/services/authentication.service';
+import { Login } from 'app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,22 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  login: Login = new Login();
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authenticate: AuthenticationService
+    ) { }
 
   ngOnInit() {
   }
+  loginForm(){
+    this.authenticate.authenticate(this.login.username, this.login.password)
+                    .subscribe( result => this.router.navigate(['/logged-in']));          
+    // console.log(this.login.username + "  " + this.login.password)
+  }
+
+  
 
 }
