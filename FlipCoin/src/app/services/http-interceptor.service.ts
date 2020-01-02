27e9f09be4f4b0186aba2589037ac8b950cba1c2
chung <chung305>
2020-01-02 16:55:44 -0500

    fixing cors issues

--- FlipCoin/src/app/services/http-interceptor.service.ts
+++ FlipCoin/src/app/services/http-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +10,21 @@ export class HttpInterceptorService  implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler){
     if(sessionStorage.getItem('username') && sessionStorage.getItem('token')){
       req = req.clone({
-        setHeaders: {
-          Authorization: sessionStorage.getItem('token'),
+        headers: new HttpHeaders({
+          'Authorization': sessionStorage.getItem('token'),
           'Access-Control-Allow-Origin' : '*',
           'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
           'Content-Type': 'application/json'
-        }
-      })
+        })
+        // setHeaders: {
+        //   Authorization: sessionStorage.getItem('token'),
+        //   'Access-Control-Allow-Origin' : '*',
+        //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        //    'Content-Type': 'application/json'
+        // }
+      });
     }
     return next.handle(req);
   }
